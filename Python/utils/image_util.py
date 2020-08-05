371bade93768887c4d06b61fd8164725115c927f
胡学东 <huxuedong@huxuedongdeMacBook-Pro.local>
2020-08-05 16:05:17 +0800

    change

--- Python/utils/image_util.py
+++ Python/utils/image_util.py
@@ -2,13 +2,16 @@ import os
 from PIL import Image
 
 
-im = Image.open("AppIcon20x20@2x~ipad.png")
-path = "C:\\Users\\huxuedong\\Downloads\\g78_pub_0403_1719_r245759\\Payload\\client.app"
+im = Image.open("3.png")
+print im.mode
+im = Image.open("AppIcon20x20@2x_1.png")
+print im.mode
+path = "/Users/huxuedong/Downloads/Payload"
 for root, dirs, files in os.walk(path):
     for name in files:
         if not name.endswith('.png'):
             continue
         file_path = os.path.join(root, name)
         im = Image.open(file_path)
-        if im.mode == 'RGBA':
-            print name
+        # if im.mode == 'RGBA':
+        #     print name
